5c859a58776b246219280d6317a526854c7b3077
anuphat-bit <anupha.t@lawasri.tru.ac.th>
2026-03-24 14:31:08 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // เพิ่มบรรทัดด้านล่างนี้เพื่อให้แสดงผลบน GitHub Pages ได้ถูกต้อง
-  base: '/GO/', 
+  base: '/GO/',
+  build: {
+    rollupOptions: {
+      input: 'index.html' // ย้ำให้ Vite รู้ว่าเริ่มที่นี่
+    }
+  }
 })
